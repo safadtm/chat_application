6623b5a450d42ac1d974e6c8ab248ffe8e842377
safadtm <safadtm2004@gmail.com>
2022-09-16 23:56:37 +0530

    Displaying Chat Contacts done

--- lib/features/chat/controller/chat_controller.dart
+++ lib/features/chat/controller/chat_controller.dart
@@ -6,6 +6,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_ui/features/auth/controller/auth_controller.dart';
 
 import 'package:whatsapp_ui/features/chat/repositories/chat_repository.dart';
+import 'package:whatsapp_ui/models/chat_contact.dart';
 
 final chatControllerProvider = Provider(
   (ref) {
@@ -20,10 +21,16 @@ final chatControllerProvider = Provider(
 class ChatController {
   final ChatRepository chatRepository;
   final ProviderRef ref;
+
   ChatController({
     required this.chatRepository,
     required this.ref,
   });
+
+  Stream<List<ChatContact>> chatContacts() {
+    return chatRepository.getChatContacts();
+  }
+
   void sendTextMessage(
       BuildContext context, String text, String recieverUserId) {
     ref.read(userDataAuthProvider).whenData(
--- lib/features/chat/repositories/chat_repository.dart
+++ lib/features/chat/repositories/chat_repository.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:uuid/uuid.dart';
 import 'package:whatsapp_ui/common/enums/message_enum.dart';
 import 'package:whatsapp_ui/common/utils/utils.dart';
+import 'package:whatsapp_ui/info.dart';
 import 'package:whatsapp_ui/models/chat_contact.dart';
 import 'package:whatsapp_ui/models/message.dart';
 import 'package:whatsapp_ui/models/user_model.dart';
@@ -24,6 +25,40 @@ class ChatRepository {
     required this.firestore,
     required this.auth,
   });
+
+  Stream<List<ChatContact>> getChatContacts() {
+    return firestore
+        .collection('users')
+        .doc(auth.currentUser!.uid)
+        .collection('chats')
+        .snapshots()
+        .asyncMap(
+      (event) async {
+        List<ChatContact> contacts = [];
+        for (var document in event.docs) {
+          var chatContact = ChatContact.fromMap(
+            document.data(),
+          );
+          var userdata = await firestore
+              .collection('users')
+              .doc(chatContact.contactId)
+              .get();
+          var user = UserModel.fromMap(userdata.data()!);
+
+          contacts.add(
+            ChatContact(
+              name: user.name,
+              profilePic: user.profilePic,
+              contactId: chatContact.contactId,
+              timeSent: chatContact.timeSent,
+              lastMessage: chatContact.lastMessage,
+            ),
+          );
+        }
+        return contacts;
+      },
+    );
+  }
 
   void _saveDataToContactSubcollection(
     UserModel sendUserData,
