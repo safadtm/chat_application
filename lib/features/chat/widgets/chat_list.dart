1e26fce9b3064b3f32f6b1d7c505562497eb8694
safadtm <safadtm2004@gmail.com>
2022-09-17 01:55:33 +0530

    Changing Online/Offline Status Done

--- lib/features/chat/widgets/chat_list.dart
+++ lib/features/chat/widgets/chat_list.dart
@@ -1,17 +1,17 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:whatsapp_ui/common/widgets/loader.dart';
 
 import 'package:whatsapp_ui/features/chat/controller/chat_controller.dart';
-import 'package:whatsapp_ui/info.dart';
 import 'package:whatsapp_ui/models/message.dart';
 import 'package:whatsapp_ui/widgets/my_message_card.dart';
 import 'package:whatsapp_ui/widgets/sender_message_card.dart';
 
-class ChatList extends ConsumerWidget {
+class ChatList extends ConsumerStatefulWidget {
   final String recieverUserId;
   const ChatList({
     Key? key,
@@ -19,15 +19,36 @@ class ChatList extends ConsumerWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _ChatListState();
+}
+
+class _ChatListState extends ConsumerState<ChatList> {
+  final ScrollController messageController = ScrollController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    messageController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return StreamBuilder<List<Message>>(
-        stream: ref.read(chatControllerProvider).chatStream(recieverUserId),
+        stream:
+            ref.read(chatControllerProvider).chatStream(widget.recieverUserId),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Loader();
           }
-
+          SchedulerBinding.instance.addPostFrameCallback(
+            (_) {
+              messageController.jumpTo(
+                messageController.position.maxScrollExtent,
+              );
+            },
+          );
           return ListView.builder(
+            controller: messageController,
             itemCount: snapshot.data!.length,
             itemBuilder: (context, index) {
               final messageData = snapshot.data![index];
